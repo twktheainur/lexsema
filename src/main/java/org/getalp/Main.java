538b704692a377135156d6e2cadc67b02e1fba88
twktheainur <twk.theainur@gmail.com>
2013-06-13 14:37:59 -0400

    Main class, directory structure

---
+++ src/main/java/org/getalp/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.getalp;
+
+/**
+ * @author tchechem
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
