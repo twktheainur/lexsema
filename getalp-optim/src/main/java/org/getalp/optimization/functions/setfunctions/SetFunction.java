a62f2b93c8cf07e5d49210bbfa0238fc2b27d8eb
twktheainur <twk.theainur@gmail.com>
2014-11-03 21:58:30 +0100

    - Moved optimisation separate project

---
+++ getalp-optim/src/main/java/org/getalp/optimization/functions/setfunctions/SetFunction.java
@@ -0,0 +1,38 @@
+package org.getalp.optimization.functions.setfunctions;
+
+
+import org.getalp.optimization.functions.Function;
+import org.getalp.optimization.functions.input.FunctionInput;
+import org.getalp.optimization.functions.setfunctions.extentions.Extension;
+import org.getalp.optimization.functions.setfunctions.input.SetFunctionInput;
+
+public abstract class SetFunction implements Function {
+    private Extension extension;
+
+    protected SetFunction() {
+    }
+
+    public double F(FunctionInput input) {
+        if (input instanceof SetFunctionInput) {
+            return F((SetFunctionInput) input);
+        } else {
+            return 0d;
+        }
+    }
+
+    public abstract double F(SetFunctionInput input);
+
+    public Extension getExtension() {
+        return extension;
+    }
+
+    public void setExtension(Extension e) {
+        extension = e;
+        e.setSetFunction(this);
+    }
+
+    public boolean isDifferentiable() {
+        return extension != null && extension.isDifferentiable();
+    }
+
+}
---
+++ getalp-optim/src/main/java/org/getalp/optimization/functions/setfunctions/input/ValueListInput.java
@@ -0,0 +1,53 @@
+package org.getalp.optimization.functions.setfunctions.input;
+
+import cern.colt.matrix.tdouble.impl.DenseDoubleMatrix1D;
+import org.getalp.optimization.functions.input.FunctionInput;
+
+import java.util.List;
+
+public class ValueListInput extends SetFunctionInput {
+
+    private List<Double> values;
+    private boolean invert;
+
+    public ValueListInput(List<Double> values, boolean invert) {
+        this.values = values;
+        this.invert = invert;
+        compute();
+    }
+
+    public ValueListInput(List<Double> values, boolean invert, boolean compute) {
+        this.values = values;
+        this.invert = invert;
+        if (compute) {
+            compute();
+        }
+    }
+
+    private void compute() {
+        setInput(new DenseDoubleMatrix1D(values.size()));
+        setValues(new DenseDoubleMatrix1D(values.size()));
+        for (int i = 0; i < values.size(); i++) {
+            getInput().setQuick(i, 1d);
+            if (invert) {
+                getValues().setQuick(i, 1 - values.get(i));
+            } else {
+                getValues().setQuick(i, values.get(i));
+            }
+        }
+        setInterval(new Interval(0, (int) getInput().size()));
+    }
+
+
+    @Override
+    public FunctionInput copy() {
+        ValueListInput nin = new ValueListInput(values, invert, false);
+        nin.setInput(getInput().copy());
+        nin.setValues(getValues().copy());
+        if (getPermutation() != null) {
+            nin.setPermutation(getPermutation());
+        }
+        return nin;
+    }
+
+}
