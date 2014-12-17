bd0b801618493c303f32534b22afffc7f352f5ff
twktheainur <twk.theainur@gmail.com>
2014-12-17 18:53:13 +0100

    -Echo with newer features

--- org.getalp.lexsema-wsd/org.getalp.lexsema-wsd-supervised/src/main/java/org/getalp/lexsema/supervised/experiments/EchoDisambiguation.java
+++ org.getalp.lexsema-wsd/org.getalp.lexsema-wsd-supervised/src/main/java/org/getalp/lexsema/supervised/experiments/EchoDisambiguation.java
@@ -9,13 +9,20 @@ import org.getalp.lexsema.io.resource.LRLoader;
 import org.getalp.lexsema.io.resource.wordnet.WordnetLoader;
 import org.getalp.lexsema.similarity.Document;
 import org.getalp.lexsema.supervised.EchoDisambiguator;
-import org.getalp.lexsema.supervised.features.extractors.*;
+import org.getalp.lexsema.supervised.features.ContextWindow;
+import org.getalp.lexsema.supervised.features.WindowLoader;
+import org.getalp.lexsema.supervised.features.extractors.AggregateLocalTextFeatureExtractor;
+import org.getalp.lexsema.supervised.features.extractors.AlignedContextFeatureExtractor;
+import org.getalp.lexsema.supervised.features.extractors.LocalCollocationFeatureExtractor;
+import org.getalp.lexsema.supervised.features.extractors.PosFeatureExtractor;
 import org.getalp.lexsema.wsd.configuration.Configuration;
 import org.getalp.lexsema.wsd.method.Disambiguator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EchoDisambiguation {
     private static Logger logger = LoggerFactory.getLogger(EchoDisambiguation.class);
@@ -23,17 +30,33 @@ public class EchoDisambiguation {
     public static void main(String[] args) throws IOException {
         TextLoader dl = new Semeval2007TextLoader("../data/senseval2007_task7/test/eng-coarse-all-words.xml").loadNonInstances(false);
         LRLoader lrloader = new WordnetLoader("../data/wordnet/2.1/dict").setHasExtendedSignature(true).setShuffle(false);
-        LocalTextFeatureExtractor lfe = new LemmaFeatureExtractor(3, 1);
-        LocalTextFeatureExtractor pfe = new PosFeatureExtractor(1, 2);
-        LocalTextFeatureExtractor tplfe = new TargetPosLemmaFeatureExtractor();
+        WindowLoader wloader = new WindowLoader("../data/indexes/windows.csv");
+        wloader.load();
+
+
+        List<ContextWindow> contextWindows = new ArrayList<>();
+        contextWindows.add(new ContextWindow(-1, -1));
+        contextWindows.add(new ContextWindow(1, 1));
+        contextWindows.add(new ContextWindow(-2, -2));
+        contextWindows.add(new ContextWindow(2, 2));
+        contextWindows.add(new ContextWindow(-2, -1));
+        contextWindows.add(new ContextWindow(-1, 1));
+        contextWindows.add(new ContextWindow(1, 2));
+        contextWindows.add(new ContextWindow(-3, -1));
+        contextWindows.add(new ContextWindow(-2, 1));
+        contextWindows.add(new ContextWindow(-1, 2));
+        contextWindows.add(new ContextWindow(1, 3));
+        LocalCollocationFeatureExtractor lcfe = new LocalCollocationFeatureExtractor(contextWindows);
+        PosFeatureExtractor pfe = new PosFeatureExtractor(3, 3);
+        AlignedContextFeatureExtractor acfe = new AlignedContextFeatureExtractor(wloader);
 
         AggregateLocalTextFeatureExtractor altfe = new AggregateLocalTextFeatureExtractor();
-        altfe.addExtractor(lfe);
+        altfe.addExtractor(lcfe);
         altfe.addExtractor(pfe);
-        altfe.addExtractor(tplfe);
+        altfe.addExtractor(acfe);
 
         //Le dernier argument est la taille de la poole de threads
-        Disambiguator disambiguator = new EchoDisambiguator("../lexsema_pkg/data/semcor_features/", altfe, 1);
+        Disambiguator disambiguator = new EchoDisambiguator("../data/supervised/", altfe, 1);
         logger.info("Loading texts");
         dl.load();
         int i = 0;
