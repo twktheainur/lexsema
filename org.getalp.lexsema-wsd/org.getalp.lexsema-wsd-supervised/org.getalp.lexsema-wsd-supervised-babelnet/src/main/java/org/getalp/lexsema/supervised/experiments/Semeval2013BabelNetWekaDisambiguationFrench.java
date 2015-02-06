f233766a6609dd9642e65bf42630c94fe2df1527
twktheainur <twk.theainur@gmail.com>
2015-02-06 13:58:34 +0100

    Bebelnet supervised WSD

---
+++ org.getalp.lexsema-wsd/org.getalp.lexsema-wsd-supervised/org.getalp.lexsema-wsd-supervised-babelnet/src/main/java/org/getalp/lexsema/supervised/experiments/Semeval2013BabelNetWekaDisambiguationFrench.java
@@ -0,0 +1,99 @@
+package org.getalp.lexsema.supervised.experiments;
+
+
+import org.getalp.lexsema.io.annotresult.ConfigurationWriter;
+import org.getalp.lexsema.io.annotresult.SemevalWriter;
+import org.getalp.lexsema.io.document.SemCorTextLoader;
+import org.getalp.lexsema.io.document.Semeval2013Task13TextLoader;
+import org.getalp.lexsema.io.document.TextLoader;
+import org.getalp.lexsema.io.resource.LRLoader;
+import org.getalp.lexsema.io.resource.babelnet.BabelNetAPILoader;
+import org.getalp.lexsema.language.Language;
+import org.getalp.lexsema.similarity.Document;
+import org.getalp.lexsema.supervised.WekaDisambiguator;
+import org.getalp.lexsema.supervised.features.*;
+import org.getalp.lexsema.supervised.features.extractors.AggregateLocalTextFeatureExtractor;
+import org.getalp.lexsema.supervised.features.extractors.LemmaFeatureExtractor;
+import org.getalp.lexsema.supervised.features.extractors.LocalCollocationFeatureExtractor;
+import org.getalp.lexsema.supervised.features.extractors.PosFeatureExtractor;
+import org.getalp.lexsema.supervised.weka.NaiveBayesSetUp;
+import org.getalp.lexsema.wsd.configuration.Configuration;
+import org.getalp.lexsema.wsd.method.Disambiguator;
+import org.getalp.lexsema.wsd.method.FirstSenseDisambiguator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Semeval2013BabelNetWekaDisambiguationFrench {
+    private static Logger logger = LoggerFactory.getLogger(Semeval2013BabelNetWekaDisambiguationFrench.class);
+
+    public static void main(String[] args) throws IOException {
+        TextLoader dl = new Semeval2013Task13TextLoader("../data/semeval-2013-task12-test-data/data/multilingual-all-words.fr.xml")
+                .loadNonInstances(false);
+        TextLoader semCor = new SemCorTextLoader("../data/semcor3.0/semcor_full.xml");
+        LRLoader lrloader = new BabelNetAPILoader(Language.FRENCH).extendedSignature(false).suffle(false).loadDefinition(false).setLoadRelated(false);
+        /*LRLoader lrloader = new WordnetLoader("../data/wordnet/3.0/dict")
+                .extendedSignature(true).suffle(true);*/
+        semCor.load();
+        WindowLoader wloader = new DocumentCollectionWindowLoader(semCor);
+        wloader.load();
+
+        logger.info("Loading texts");
+        dl.load();
+
+        logger.info("Extracting features");
+        List<ContextWindow> contextWindows = new ArrayList<>();
+        contextWindows.add(new ContextWindow(-1, -1));
+        contextWindows.add(new ContextWindow(1, 1));
+        contextWindows.add(new ContextWindow(-2, -2));
+        contextWindows.add(new ContextWindow(2, 2));
+        contextWindows.add(new ContextWindow(-2, -1));
+        contextWindows.add(new ContextWindow(-1, 1));
+        contextWindows.add(new ContextWindow(1, 2));
+        contextWindows.add(new ContextWindow(-3, -1));
+        contextWindows.add(new ContextWindow(-2, 1));
+        contextWindows.add(new ContextWindow(-1, 2));
+        contextWindows.add(new ContextWindow(1, 3));
+        LocalCollocationFeatureExtractor lcfe = new LocalCollocationFeatureExtractor(contextWindows);
+        PosFeatureExtractor pfe = new PosFeatureExtractor(5, 5);
+        LemmaFeatureExtractor acfe = new LemmaFeatureExtractor(5, 5);
+
+
+        AggregateLocalTextFeatureExtractor altfe = new AggregateLocalTextFeatureExtractor();
+        altfe.addExtractor(lcfe);
+        altfe.addExtractor(pfe);
+        altfe.addExtractor(acfe);
+
+        TrainingDataExtractor trainingDataExtractor = new BabelLemmaTranslateSemCorTrainingDataExtractor(altfe, new File("../data/mapping/semcor_babelnet_mapping.fr.csv"), Language.FRENCH);
+        //TrainingDataExtractor trainingDataExtractor = new SemCorTrainingDataExtractor(altfe);
+        //trainingDataExtractor.extract(semCor);
+
+        //Le dernier argument est la taille de la poole de threads
+        // pour changer echo ou echo 2 changer dans EchoLexicalEntryDisambiguator
+        Disambiguator disambiguator = new WekaDisambiguator("../data/supervised", new NaiveBayesSetUp(true, true), altfe, 2, trainingDataExtractor);
+        Disambiguator firstSense = new FirstSenseDisambiguator("../data/semeval-2013-task12-test-data/keys/baselines/babelnet/semcor.first-sense.fr.key");
+        int i = 0;
+        if (args.length == 1) {
+            i = Integer.valueOf(args[0]) - 1;
+        }
+        for (Document d : dl) {
+            System.err.println("Starting document " + d.getId());
+            System.err.println("\tLoading senses...");
+            lrloader.loadSenses(d);
+
+            //Configuration c = disambiguator.disambiguate(d);
+            //c = firstSense.disambiguate(d,c);
+            Configuration c = firstSense.disambiguate(d);
+            ConfigurationWriter sw = new SemevalWriter(d.getId() + ".ans");
+            System.err.println("\n\tWriting results...");
+            sw.write(d, c.getAssignments());
+            System.err.println("done!");
+        }
+        disambiguator.release();
+        //disambiguator.release();
+    }
+}
---
+++ org.getalp.lexsema-wsd/org.getalp.lexsema-wsd-supervised/org.getalp.lexsema-wsd-supervised-babelnet/src/main/java/org/getalp/lexsema/supervised/experiments/Semeval2013WordnetWekaDisambiguation.java
@@ -0,0 +1,93 @@
+package org.getalp.lexsema.supervised.experiments;
+
+
+import org.getalp.lexsema.io.annotresult.ConfigurationWriter;
+import org.getalp.lexsema.io.annotresult.SemevalWriter;
+import org.getalp.lexsema.io.document.SemCorTextLoader;
+import org.getalp.lexsema.io.document.Semeval2013Task13TextLoader;
+import org.getalp.lexsema.io.document.TextLoader;
+import org.getalp.lexsema.io.resource.LRLoader;
+import org.getalp.lexsema.io.resource.wordnet.WordnetLoader;
+import org.getalp.lexsema.similarity.Document;
+import org.getalp.lexsema.supervised.WekaDisambiguator;
+import org.getalp.lexsema.supervised.features.*;
+import org.getalp.lexsema.supervised.features.extractors.AggregateLocalTextFeatureExtractor;
+import org.getalp.lexsema.supervised.features.extractors.LemmaFeatureExtractor;
+import org.getalp.lexsema.supervised.features.extractors.LocalCollocationFeatureExtractor;
+import org.getalp.lexsema.supervised.features.extractors.PosFeatureExtractor;
+import org.getalp.lexsema.supervised.weka.NaiveBayesSetUp;
+import org.getalp.lexsema.wsd.configuration.Configuration;
+import org.getalp.lexsema.wsd.method.Disambiguator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Semeval2013WordnetWekaDisambiguation {
+    private static Logger logger = LoggerFactory.getLogger(Semeval2013WordnetWekaDisambiguation.class);
+
+    public static void main(String[] args) throws IOException {
+        TextLoader dl = new Semeval2013Task13TextLoader("../data/semeval-2013-task12-test-data/data/multilingual-all-words.en.xml")
+                .loadNonInstances(false);
+        TextLoader semCor = new SemCorTextLoader("../data/semcor3.0/semcor_full.xml");
+        //LRLoader lrloader = new BabelNetAPILoader(Language.ENGLISH).extendedSignature(false).suffle(false).loadDefinition(false).setLoadRelated(false);
+        LRLoader lrloader = new WordnetLoader("../data/wordnet/3.0/dict")
+                .extendedSignature(true).suffle(true);
+        semCor.load();
+        WindowLoader wloader = new DocumentCollectionWindowLoader(semCor);
+        wloader.load();
+
+        logger.info("Loading texts");
+        dl.load();
+
+        List<ContextWindow> contextWindows = new ArrayList<>();
+        contextWindows.add(new ContextWindow(-1, -1));
+        contextWindows.add(new ContextWindow(1, 1));
+        contextWindows.add(new ContextWindow(-2, -2));
+        contextWindows.add(new ContextWindow(2, 2));
+        contextWindows.add(new ContextWindow(-2, -1));
+        contextWindows.add(new ContextWindow(-1, 1));
+        contextWindows.add(new ContextWindow(1, 2));
+        contextWindows.add(new ContextWindow(-3, -1));
+        contextWindows.add(new ContextWindow(-2, 1));
+        contextWindows.add(new ContextWindow(-1, 2));
+        contextWindows.add(new ContextWindow(1, 3));
+        LocalCollocationFeatureExtractor lcfe = new LocalCollocationFeatureExtractor(contextWindows);
+        PosFeatureExtractor pfe = new PosFeatureExtractor(5, 5);
+        LemmaFeatureExtractor acfe = new LemmaFeatureExtractor(5, 5);
+
+
+        AggregateLocalTextFeatureExtractor altfe = new AggregateLocalTextFeatureExtractor();
+        altfe.addExtractor(lcfe);
+        altfe.addExtractor(pfe);
+        altfe.addExtractor(acfe);
+
+        logger.info("Extracting features.");
+        //TrainingDataExtractor trainingDataExtractor = new BabelLemmaTranslateSemCorTrainingDataExtractor(altfe,new File("../data/mapping/semcor_babelnet_mapping.en.csv"));TrainingDataExtractor trainingDataExtractor = new BabelLemmaTranslateSemCorTrainingDataExtractor(altfe,new File("../data/mapping/semcor_babelnet_mapping.en.csv"));
+        TrainingDataExtractor trainingDataExtractor = new SemCorTrainingDataExtractor(altfe);
+        trainingDataExtractor.extract(semCor);
+
+        //Le dernier argument est la taille de la poole de threads
+        // pour changer echo ou echo 2 changer dans EchoLexicalEntryDisambiguator
+        Disambiguator disambiguator = new WekaDisambiguator("../data/supervised", new NaiveBayesSetUp(Boolean.parseBoolean(args[0]), Boolean.parseBoolean(args[1])), altfe, Integer.parseInt(args[2]), trainingDataExtractor);
+        int i = 0;
+        if (args.length == 1) {
+            i = Integer.valueOf(args[0]) - 1;
+        }
+        for (Document d : dl) {
+            System.err.println("Starting document " + d.getId());
+            System.err.println("\tLoading senses...");
+            lrloader.loadSenses(d);
+
+            Configuration c = disambiguator.disambiguate(d);
+            ConfigurationWriter sw = new SemevalWriter(d.getId() + ".ans");
+            System.err.println("\n\tWriting results...");
+            sw.write(d, c.getAssignments());
+            System.err.println("done!");
+        }
+        disambiguator.release();
+        disambiguator.release();
+    }
+}
