5677d463625de5bb1a267deea0c961170d02ab24
Bastien Marque <marque@axum.imag.fr>
2015-06-04 09:15:10 +0200

    test
    Former-commit-id: e682a1ae2e53e35b95fa8f1537550b98aaa58791

--- org.getalp.lexsema-wsd/org.getalp.lexsema-wsd-core/src/main/java/org/getalp/lexsema/wsd/experiments/CombinedDisambiguation.java
+++ org.getalp.lexsema-wsd/org.getalp.lexsema-wsd-core/src/main/java/org/getalp/lexsema/wsd/experiments/CombinedDisambiguation.java
@@ -27,7 +27,7 @@ public class CombinedDisambiguation {
 
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        System.out.println("jambon2");
+        System.out.println("jambon1");
         TextLoader dl = new Semeval2007TextLoader("../data/senseval2007_task7/test/eng-coarse-all-words.xml").loadNonInstances(true);
         LRLoader lrloader = new WordnetLoader("../data/wordnet/2.1/dict")
 		.extendedSignature(true)
