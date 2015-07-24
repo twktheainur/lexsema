cacbd69c4aa630b647b468de1112ab0f366b697c
Loïc Vial <contact.coyl@gmail.com>
2015-07-24 16:02:28 +0200

    Rename project lexsema-nif in lexsema-ws (WebServices) + Remove all dependencies to nlp2rdf in NIF web service
    Former-commit-id: 579cad58e51410775b12df2cbb8798d36ad422b2

---
+++ org.getalp.lexsema-io/org.getalp.lexsema-io-core/src/main/java/org/getalp/lexsema/io/nif/NIFURI.java
@@ -0,0 +1,33 @@
+package org.getalp.lexsema.io.nif;
+
+public class NIFURI
+{
+    public static String prefix = "http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#";
+    
+    public static String isString = prefix + "isString";
+
+    public static String beginIndex = prefix + "beginIndex";
+
+    public static String endIndex = prefix + "endIndex";
+
+    public static String anchorOf = prefix + "anchorOf";
+
+    public static String referenceContext = prefix + "referenceContext";
+
+    public static String previousWord = prefix + "previousWord";
+
+    public static String nextWord = prefix + "nextWord";
+
+    public static String firstWord = prefix + "firstWord";
+
+    public static String lastWord = prefix + "lastWord";
+
+    public static String word = prefix + "word";
+
+    public static String Context = prefix + "Context";
+
+    public static String Word = prefix + "Word";
+    
+    public static String RFC5147String = prefix + "RFC5147String";
+
+}
---
+++ org.getalp.lexsema-ws/src/main/java/org/getalp/lexsema/ws/WebServiceServlet.java
@@ -0,0 +1,39 @@
+package org.getalp.lexsema.ws;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Abstract Servlet class which is useful by two aspects : 
+ * - We process GET and POST requests the same way
+ * - The processing method "handle()" can throw exceptions 
+ */
+public abstract class WebServiceServlet extends HttpServlet
+{
+    private static final long serialVersionUID = 1L;
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    {
+        handleWithoutException(request, response);
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+    {
+        handleWithoutException(request, response);
+    }
+
+    private void handleWithoutException(HttpServletRequest request, HttpServletResponse response)
+    {
+        try
+        {
+            handle(request, response);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    
+    protected abstract void handle(HttpServletRequest request, HttpServletResponse response) throws Exception;
+}
