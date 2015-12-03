58af649f0d46fee9c35120daa2428eb0d640a7e3
schwab <didier.schwab@imag.fr>
2015-12-03 17:51:41 +0100

    lexical access
    Former-commit-id: fdc1035f642d7c81f26d4564314795a8e9576edc

---
+++ org.getalp.lexsema-examples/src/main/java/org/getalp/lexsema/examples/Word2VecLexicalAccess.java
@@ -0,0 +1,42 @@
+package org.getalp.lexsema.examples;
+
+import org.getalp.lexsema.io.word2vec.MultilingualSerializedModelWord2VecLoader;
+import org.getalp.lexsema.io.word2vec.MultilingualWord2VecLoader;
+import org.getalp.lexsema.io.word2vec.SerializedModelWord2VecLoader;
+import org.getalp.lexsema.io.word2vec.Word2VecLoader;
+import org.getalp.lexsema.similarity.measures.SimilarityMeasure;
+import org.getalp.lexsema.similarity.measures.word2vec.Word2VecGlossCosineSimilarity;
+import org.getalp.lexsema.similarity.signatures.SemanticSignature;
+import org.getalp.lexsema.similarity.signatures.SemanticSignatureImpl;
+import org.getalp.lexsema.util.Language;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+public final class Word2VecLexicalAccess {
+    private static Logger logger = LoggerFactory.getLogger(Word2VecLexicalAccess.class);
+
+    public static void main(String[] args) throws IOException {
+
+        if (args.length <2) {
+            usage();
+        }
+        SemanticSignature signature1 = new SemanticSignatureImpl(args[0].replaceAll("\\p{Punct}","").toLowerCase().trim());
+        SemanticSignature signature2 = new SemanticSignatureImpl(args[1].replaceAll("\\p{Punct}","").toLowerCase().trim());
+
+        Word2VecLoader word2VecLoader = new SerializedModelWord2VecLoader();
+        word2VecLoader.loadGoogle(new File(args[2]),true);
+
+        SimilarityMeasure similarityMeasure = new Word2VecGlossCosineSimilarity(word2VecLoader.getWord2Vec(),true);
+        double sim = similarityMeasure.compute(signature1, signature2);
+        String output = String.format("The similarity between \"%s\" and \"%s\" is %s", signature1.toString(), signature2.toString(), sim);
+        logger.info(output);
+    }
+
+    private static void usage() {
+        logger.error("Usage -- org.getalp.lexsema.examples.TextSimilarity \"String1\" \"String2\"");
+        System.exit(1);
+    }
+}
