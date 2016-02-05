cf7a873c53e7622a7ba6512ac7d0918f6b4afa13
Loïc Vial <contact.coyl@gmail.com>
2016-02-05 15:12:47 +0100

    Adapt DictionaryCreation to take optional Distributed flag
    Former-commit-id: 594e45e44f033e67f42c9292fbe84d1f39c1b022

--- org.getalp.lexsema-wsd/org.getalp.lexsema-wsd-core/src/main/java/org/getalp/lexsema/wsd/experiments/DictionaryCreation.java
+++ org.getalp.lexsema-wsd/org.getalp.lexsema-wsd-core/src/main/java/org/getalp/lexsema/wsd/experiments/DictionaryCreation.java
@@ -6,8 +6,15 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStreamWriter;
+import java.net.URI;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -22,12 +29,16 @@ import org.getalp.lexsema.io.document.loader.CorpusLoader;
 import org.getalp.lexsema.io.document.loader.SemCorCorpusLoader;
 import org.getalp.lexsema.io.document.loader.Semeval2007CorpusLoader;
 import org.getalp.lexsema.io.document.loader.WordnetGlossTagCorpusLoader;
+import org.getalp.lexsema.io.resource.LRLoader;
+import org.getalp.lexsema.io.resource.dictionary.DictionaryLRLoader;
 import org.getalp.lexsema.io.resource.wordnet.WordnetLoader;
 import org.getalp.lexsema.similarity.Sense;
 import org.getalp.lexsema.similarity.Text;
 import org.getalp.lexsema.similarity.Word;
 import org.getalp.lexsema.similarity.signatures.enrichment.SignatureEnrichment;
 import org.getalp.lexsema.similarity.signatures.enrichment.Word2VecLocalSignatureEnrichment;
+import org.getalp.lexsema.util.distribution.SparkSingleton;
+import org.getalp.lexsema.wsd.experiments.distributed.DistributedEmbeddingsDictionaryCreation;
 import org.getalp.lexsema.io.thesaurus.AnnotatedTextThesaurusImpl;
 import org.getalp.lexsema.io.word2vec.SerializedModelWord2VecLoader;
 import org.getalp.lexsema.io.word2vec.Word2VecLoader;
@@ -35,33 +46,39 @@ import org.getalp.lexsema.io.word2vec.Word2VecLoader;
 public class DictionaryCreation
 {
     public static String wordnetPath = "../data/wordnet/2.1/dict/";
-    
+
+    public static String wordnetResourcePath = "/wordnet_full_dict.xml";
+
     public static String semCorPath = "../data/semcor2.1/all.xml";
 
     public static String dsoPath = "../data/dso/";
-    
+
     public static String docPath = "../data/senseval2007_task7/test/eng-coarse-all-words.xml";
 
+    public static String docResourcePath = "/semeval2007/eng-coarse-all-words.xml";
+
     public static String wordnetGlossTagPath = "../data/wordnet/3.0/glosstag/";
-    
+
     public static String gmbPath = "../data/gmb-2.2.0/";
-    
+
     public static String word2vecPath = "../data/word2vec/";
-    
+
+    public static String word2vecResourcePath = "/word2vec/eng/";
+
     public static Dictionary wordnet = new Dictionary(new File(wordnetPath));
-    
+
     public static CorpusLoader semCor = new SemCorCorpusLoader(semCorPath);
-    
+
     public static CorpusLoader dso = new OldDSOCorpusLoader(dsoPath, wordnetPath);
 
     public static CorpusLoader wordnetGlossTag = new WordnetGlossTagCorpusLoader(wordnetGlossTagPath);
 
     public static CorpusLoader gmb = new GMBCorpusLoader(gmbPath, wordnet);
-    
+
     public static CorpusLoader corpus = initializeSemeval2007Corpus();
-    
+
     public static Word2VecLoader word2VecLoader = new SerializedModelWord2VecLoader();
-    
+
     public static SignatureEnrichment signatureEnrichment = null;
 
     public static boolean semCorIsLoaded = false;
@@ -71,37 +88,62 @@ public class DictionaryCreation
     public static boolean wordnetGlossTagIsLoaded = false;
 
     public static boolean gmbgIsLoaded = false;
-    
+
     public static boolean word2vecIsLoaded = false;
-    
+
     public static boolean corpusIsLoaded = false;
-    
+
     public static void main(String[] args) throws Exception
     {
-        writeDictionary(true, true, true, true, true, true, false, false, false, false, 100, true, 20, true, "../data/lesk_dict/semeval2007task7/w2v20");
-        writeDictionary(true, true, true, true, true, true, false, false, false, false, 100, true, 15, true, "../data/lesk_dict/semeval2007task7/w2v15");
-        writeDictionary(true, true, true, true, true, true, false, false, false, false, 100, true, 5, true, "../data/lesk_dict/semeval2007task7/w2v5");
-        writeDictionary(true, true, true, true, true, true, false, false, false, false, 100, true, 3, true, "../data/lesk_dict/semeval2007task7/w2v3");
+        writeDictionary(true, true, true, true, true, true, false, false, false, false, 100, true, 20, true, true, "../data/lesk_dict/semeval2007task7/w2v20");
+        writeDictionary(true, true, true, true, true, true, false, false, false, false, 100, true, 15, true, true, "../data/lesk_dict/semeval2007task7/w2v15");
+        writeDictionary(true, true, true, true, true, true, false, false, false, false, 100, true, 5, true, true, "../data/lesk_dict/semeval2007task7/w2v5");
+        writeDictionary(true, true, true, true, true, true, false, false, false, false, 100, true, 3, true, true, "../data/lesk_dict/semeval2007task7/w2v3");
     }
-    
+
     public static void writeDictionary(boolean definitions, boolean extendedDefinitions, 
-                                        boolean stopWords, boolean stemming, 
-                                        boolean index, boolean shuffle, 
-                                        boolean useSemCorThesaurus, 
-                                        boolean useDSOThesaurus, 
-                                        boolean useWordnetGlossTag,
-                                        boolean useGMBThesaurus,
-                                        int numberOfWordsFromThesauri,
-                                        boolean useWord2Vec,
-                                        int numberOfWordsFromWord2Vec,
-                                        boolean limitToCorpus, 
-                                        String newDictPath) throws Exception
+            boolean stopWords, boolean stemming, 
+            boolean index, boolean shuffle, 
+            boolean useSemCorThesaurus, 
+            boolean useDSOThesaurus, 
+            boolean useWordnetGlossTag,
+            boolean useGMBThesaurus,
+            int numberOfWordsFromThesauri,
+            boolean useWord2Vec,
+            int numberOfWordsFromWord2Vec,
+            boolean limitToCorpus, 
+            boolean distributed,
+            String newDictPath) throws Exception
+    {    
+        if (distributed) writeDictionaryDistributed(definitions, extendedDefinitions, stopWords, stemming, index, shuffle, useSemCorThesaurus, useDSOThesaurus, useWordnetGlossTag, useGMBThesaurus, numberOfWordsFromThesauri, useWord2Vec, numberOfWordsFromWord2Vec, limitToCorpus, newDictPath);
+        else writeDictionaryNotDistributed(definitions, extendedDefinitions, stopWords, stemming, index, shuffle, useSemCorThesaurus, useDSOThesaurus, useWordnetGlossTag, useGMBThesaurus, numberOfWordsFromThesauri, useWord2Vec, numberOfWordsFromWord2Vec, limitToCorpus, newDictPath);
+    }
+
+    public static void writeDictionaryDistributed(boolean definitions, boolean extendedDefinitions, 
+            boolean stopWords, boolean stemming, 
+            boolean index, boolean shuffle, 
+            boolean useSemCorThesaurus, 
+            boolean useDSOThesaurus, 
+            boolean useWordnetGlossTag,
+            boolean useGMBThesaurus,
+            int numberOfWordsFromThesauri,
+            boolean useWord2Vec,
+            int numberOfWordsFromWord2Vec,
+            boolean limitToCorpus, 
+            String newDictPath) throws Exception
     {
         System.out.println("Building dictionary " + newDictPath + "...");
-        
-        WordnetLoader lrloader = new WordnetLoader(wordnet);
-        ArrayList<Text> corpora = new ArrayList<Text>();
-        
+
+        SparkSingleton.initialize("spark://localhost:12345", "DictionaryCreation");
+
+        File modelDir = materializeModel(word2vecResourcePath);
+        Word2VecLoader word2VecLoader = new SerializedModelWord2VecLoader();
+        word2VecLoader.loadGoogle(modelDir, true, true);
+        WordVectors vectors = word2VecLoader.getWordVectors();
+        SignatureEnrichment w2vSigEnr = new Word2VecLocalSignatureEnrichment(vectors, numberOfWordsFromWord2Vec);
+
+        LRLoader lrloader = new DictionaryLRLoader(DistributedEmbeddingsDictionaryCreation.class.getResourceAsStream(wordnetResourcePath), false, w2vSigEnr);
+
         lrloader.loadDefinitions(definitions);
         lrloader.extendedSignature(extendedDefinitions);
         lrloader.loadRelated(extendedDefinitions);
@@ -109,7 +151,46 @@ public class DictionaryCreation
         lrloader.shuffle(shuffle);
         lrloader.filterStopWords(stopWords);
         lrloader.stemming(stemming);
-        
+        lrloader.distributed(true);
+
+        CorpusLoader corpusLoader = new Semeval2007CorpusLoader(DistributedEmbeddingsDictionaryCreation.class.getResourceAsStream(docResourcePath));
+        corpusLoader.load();
+
+        for (Text document : corpusLoader) {
+            lrloader.loadSenses(document);
+        }
+
+        DocumentDictionaryWriter writer = new DocumentDictionaryWriter(corpus);
+        writer.writeDictionary(new File(newDictPath));
+    }
+
+    public static void writeDictionaryNotDistributed(boolean definitions, boolean extendedDefinitions, 
+            boolean stopWords, boolean stemming, 
+            boolean index, boolean shuffle, 
+            boolean useSemCorThesaurus, 
+            boolean useDSOThesaurus, 
+            boolean useWordnetGlossTag,
+            boolean useGMBThesaurus,
+            int numberOfWordsFromThesauri,
+            boolean useWord2Vec,
+            int numberOfWordsFromWord2Vec,
+            boolean limitToCorpus, 
+            String newDictPath) throws Exception
+    {
+        System.out.println("Building dictionary " + newDictPath + "...");
+
+        WordnetLoader lrloader = new WordnetLoader(wordnet);
+        ArrayList<Text> corpora = new ArrayList<Text>();
+
+        lrloader.loadDefinitions(definitions);
+        lrloader.extendedSignature(extendedDefinitions);
+        lrloader.loadRelated(extendedDefinitions);
+        lrloader.index(index);
+        lrloader.shuffle(shuffle);
+        lrloader.filterStopWords(stopWords);
+        lrloader.stemming(stemming);
+        lrloader.distributed(false);
+
         if (useSemCorThesaurus)
         {
             if (!semCorIsLoaded) 
@@ -122,7 +203,7 @@ public class DictionaryCreation
                 corpora.add(corpusText);
             }
         }
-        
+
         if (useDSOThesaurus)
         {
             if (!dsoIsLoaded) 
@@ -135,7 +216,7 @@ public class DictionaryCreation
                 corpora.add(corpusText);
             }
         }
-        
+
         if (useWordnetGlossTag)
         {
             if (!wordnetGlossTagIsLoaded) 
@@ -148,7 +229,7 @@ public class DictionaryCreation
                 corpora.add(corpusText);
             }
         }
-        
+
         if (useGMBThesaurus)
         {
             if (!gmbgIsLoaded) 
@@ -161,26 +242,26 @@ public class DictionaryCreation
                 corpora.add(corpusText);
             }
         }
-        
+
         lrloader.addThesaurus(new AnnotatedTextThesaurusImpl(corpora, numberOfWordsFromThesauri));
-        
+
         if (useWord2Vec)
         {
-        	if (!word2vecIsLoaded)
-        	{
+            if (!word2vecIsLoaded)
+            {
                 word2VecLoader.loadGoogle(new File(word2vecPath), true, true);
                 signatureEnrichment = new Word2VecLocalSignatureEnrichment(word2VecLoader.getWordVectors(), numberOfWordsFromWord2Vec);
                 word2vecIsLoaded = true;
-        	}
-        	lrloader.signatureEnrichment(signatureEnrichment);
+            }
+            lrloader.signatureEnrichment(signatureEnrichment);
         }
-        
+
         if (limitToCorpus)
         {
             if (!corpusIsLoaded)
             {
-            	corpus.load();
-            	corpusIsLoaded = true;
+                corpus.load();
+                corpusIsLoaded = true;
             }
             for (Text txt : corpus)
             {
@@ -200,12 +281,12 @@ public class DictionaryCreation
             bw.newLine();
             for (Word word : senses.keySet())
             {
-            	bw.write(String.format("<word tag=\"%s%%%s\">", word.getLemma(), word.getPartOfSpeech()));
-            	bw.newLine();
+                bw.write(String.format("<word tag=\"%s%%%s\">", word.getLemma(), word.getPartOfSpeech()));
+                bw.newLine();
                 for (Sense sense : senses.get(word))
                 {
-                	bw.write("<sense>");
-                	bw.newLine();
+                    bw.write("<sense>");
+                    bw.newLine();
                     bw.write(String.format("<ids>%s</ids>", sense.getId()));
                     bw.newLine();
                     bw.write("<def>");
@@ -224,19 +305,42 @@ public class DictionaryCreation
             bw.close();
             fos.flush();
             fos.close();
-        }
-        
+        }  
     }
-    
+
     public static CorpusLoader initializeSemeval2007Corpus()
     {
-    	try
-    	{
-			return new Semeval2007CorpusLoader(new FileInputStream(docPath));
-		}
-    	catch (FileNotFoundException e)
-    	{
-			throw new Error(e);
-		}
+        try
+        {
+            return new Semeval2007CorpusLoader(new FileInputStream(docPath));
+        }
+        catch (FileNotFoundException e)
+        {
+            throw new Error(e);
+        }
+    }
+
+    public static File materializeModel(String resourceURI) throws IOException {
+
+        String mURI = DistributedEmbeddingsDictionaryCreation.class.getResource(String.format("%s/model.bin", resourceURI)).toString();
+
+        Path targetModelDir;
+        final Map<String, String> env = new HashMap<>();
+        String[] array;
+        FileSystem resourceFileSystem;
+        if(mURI.contains("!")){
+            array = mURI.split("!");
+            resourceFileSystem = FileSystems.newFileSystem(URI.create(array[0]), env);
+        } else {
+            array = mURI.split(":");
+            resourceFileSystem = FileSystems.getDefault();
+        }
+        final Path path = resourceFileSystem.getPath(array[1]);
+        try (InputStream inputStream = Files.newInputStream(path)) {
+            targetModelDir = Files.createTempDirectory("materializedResource");
+            Path targetModel = targetModelDir.resolve("model.bin");
+            Files.copy(inputStream,targetModel);
+        }
+        return targetModelDir.toFile();
     }
 }
