a56fd2f0fa4c202dcbda5987e0a71eee6b1b3291
Loïc Vial <contact.coyl@gmail.com>
2016-02-19 14:28:09 +0100

    update w2v ws
    Former-commit-id: 7c79c487721616b2f7e2977d666c9cd266fbd8ad

--- org.getalp.lexsema-ws/src/main/java/org/getalp/lexsema/ws/w2v/Word2VecWebService.java
+++ org.getalp.lexsema-ws/src/main/java/org/getalp/lexsema/ws/w2v/Word2VecWebService.java
@@ -14,54 +14,138 @@ import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
 import org.deeplearning4j.models.embeddings.wordvectors.WordVectors;
 import org.getalp.lexsema.ws.core.WebServiceServlet;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.jblas.NDArray;
 import org.nd4j.linalg.ops.transforms.Transforms;
 
 public class Word2VecWebService extends WebServiceServlet
 {
     private static WordVectors word2vec = null; 
     
-    private static String default_path = "/home/viall/current/data/word2vec/model_large.bin";
+    private static boolean word2vecIsLoaded = false;
     
-    @Override
+    private static final String default_path = "/home/viall/current/data/word2vec/model_large.bin";
+    
     protected void handle(HttpServletRequest request, HttpServletResponse response) throws Exception
     {
+        setHeaders(request, response);
         String what = request.getParameter("what");
-        if (what.equals("get_word_vector"))
+        if (what == null)
         {
-            String word = request.getParameter("word");
-            //response.getWriter().println("getting vector of word " + word + "...");
-            double[] vector = word2vec.getWordVector(word);
-            response.getWriter().println(Arrays.toString(vector));
+            handleWhatNull(request, response);
+        }
+        else if (what.equals("get_word_vector"))
+        {
+            handleGetWordVector(request, response);
         }
         else if (what.equals("get_most_similar_words"))
         {
-            String word = request.getParameter("word");
-            int n = Integer.parseInt(request.getParameter("n"));
-            //response.getWriter().println("getting " + n + " most similar words to " + word + "...");
-            Collection<String> most_similar_words = getMostSimilarWord(word, n);
-            response.getWriter().println(most_similar_words.toString());
+            handleGetMostSimilarWords(request, response);
         }
         else if (what.equals("load_model"))
         {
-            String path = request.getParameter("path");
-            word2vec = loadWord2vec(path);
-            if (word2vec == null)
-            {
-                response.getWriter().println("fail");
-            }
-            else
-            {
-                response.getWriter().println("success");
-            }
+            handleLoadModel(request, response);
+        }
+        else if (what.equals("load_default_model"))
+        {
+            handleLoadDefaultModel(request, response);
+        }
+        else
+        {
+            handleWhatInvalid(request, response);
         }
     }
 
-    private Collection<String> getMostSimilarWord(String zeWordAsString, int topN) 
+    private void setHeaders(HttpServletRequest request, HttpServletResponse response)
     {
-        return getMostSimilarWord(word2vec.getWordVectorMatrix(zeWordAsString), topN);
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "*");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setContentType("text");
     }
     
-    private Collection<String> getMostSimilarWord(INDArray zeWord, int topN) 
+    private void handleWhatNull(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+        writeErrorParameterNull(response, "what");
+    }
+    
+    private void handleGetWordVector(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+        if (!word2vecIsLoaded) { writeErrorWord2vecNotLoaded(response); return; }
+        String word = request.getParameter("word");
+        if (word == null) { writeErrorParameterNull(response, "word"); return; }
+        double[] vector = word2vec.getWordVector(word);
+        response.getWriter().println(Arrays.toString(vector));
+    }
+    
+    private void handleGetMostSimilarWords(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+        if (!word2vecIsLoaded) { writeErrorWord2vecNotLoaded(response); return; }
+        String word = request.getParameter("word");
+        String vector = request.getParameter("vector");
+        if (word == null && vector == null) { writeErrorParameterNull(response, "word / vector"); return; }
+        String nAsStr = request.getParameter("n");
+        if (nAsStr == null) nAsStr = "1";
+        int n = Integer.parseInt(nAsStr);
+        if (word != null)
+        {
+            Collection<String> most_similar_words = getMostSimilarWords(word, n);
+            response.getWriter().println(most_similar_words.toString());
+        }
+        else if (vector != null)
+        {
+            String[] strValues = vector.replace("[", "").replace("]", "").split(", ");
+            double[] vectord = new double[strValues.length];
+            for (int i = 0 ; i < vectord.length ; i++) vectord[i] = Double.parseDouble(strValues[i]);
+            Collection<String> most_similar_words = getMostSimilarWords(vectord, n);
+            response.getWriter().println(most_similar_words.toString());
+        }
+    }
+    
+    private void handleLoadModel(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+        String path = request.getParameter("path");
+        if (path == null) { writeErrorParameterNull(response, "path"); return; }
+        word2vec = loadWord2vec(path);
+        if (word2vec == null) response.getWriter().println("fail");
+        else response.getWriter().println("success");
+    }
+
+    private void handleLoadDefaultModel(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+        word2vec = loadWord2vec(default_path);
+        if (word2vec == null) response.getWriter().println("fail");
+        else response.getWriter().println("success");
+    }
+
+    private void handleWhatInvalid(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+        response.getWriter().println("Error: parameter \"what\" invalid.");
+    }
+    
+    private void writeErrorWord2vecNotLoaded(HttpServletResponse response) throws IOException
+    {
+        response.getWriter().println("Error: word2vec is not loaded.");
+    }
+    
+    private void writeErrorParameterNull(HttpServletResponse response, String parameterName) throws IOException
+    {
+        response.getWriter().println("Error: parameter \"" + parameterName + "\" missing.");
+    }
+    
+    private Collection<String> getMostSimilarWords(String zeWord, int topN) 
+    {
+        return getMostSimilarWords(word2vec.getWordVectorMatrix(zeWord), topN);
+    }
+
+    private Collection<String> getMostSimilarWords(double[] zeWord, int topN) 
+    {
+        double[][] zeWord2D = new double[1][zeWord.length];
+        zeWord2D[0] = zeWord;
+        return getMostSimilarWords(new NDArray(zeWord2D), topN);
+    }
+    
+    private Collection<String> getMostSimilarWords(INDArray zeWord, int topN) 
     {
         PairStringDouble[] zenearests = new PairStringDouble[topN];
         for (int i = 0 ; i < topN ; i++) zenearests[i] = new PairStringDouble("", 0.0);
