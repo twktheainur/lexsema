250999366a8221518e2286e71349d3be1c3c23b5
Loïc Vial <contact.coyl@gmail.com>
2016-04-05 16:26:28 +0200

    update xp
    Former-commit-id: 244da3360dea4c463fe828540c76aee326094105

--- org.getalp.lexsema-ws/src/main/java/org/getalp/lexsema/ws/wsdforsmt/WSDForSMTWebService1.java
+++ org.getalp.lexsema-ws/src/main/java/org/getalp/lexsema/ws/wsdforsmt/WSDForSMTWebService1.java
@@ -82,7 +82,7 @@ public class WSDForSMTWebService1  extends WebServiceServlet
                     }
                 }
             }
-            System.out.println("\"" + txt.getWord(i) + "\" [" + outputArray[i] + "]");
+            System.out.println("\"" + txt.getWord(i).getSurfaceForm() + "\" [" + outputArray[i] + "]");
         }
         String output = Arrays.toString(outputArray);
         System.out.println("Writing output of size " + output.length() + "...");
